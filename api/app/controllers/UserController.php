e3910c7c3f63e3634d57fa84fb5e06ab2b260475
jvrtdev <joaovitorromanoteixeira@gmail.com>
2024-04-01 16:59:04 -0300

    :sparkles: feat: funcao de listar carros

--- api/app/controllers/UserController.php
+++ api/app/controllers/UserController.php
@@ -1,12 +1,36 @@
 <?php
 namespace App\Controllers;
+
+use App\Database;
+use PDO;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class UserController {
-        public function hello(Request $request, Response $response)
-        {   
-            $response->getBody()->write("Hello world!");
-            return $response;
-        }
+    public function hello(Request $request, Response $response)
+    {   
+        $response->getBody()->write("Hello world!");
+        return $response;
+    }
+    
+    public function carros(Request $request, Response $response) 
+    {
+        $database = new Database;
+        $pdo = $database->getConnection();
+
+        $stmt = $pdo->query('SELECT * FROM carros');
+
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+        
+        
+        $body = json_encode($data);  
+        
+        $response->getBody()->write($body);
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+
+
 }
