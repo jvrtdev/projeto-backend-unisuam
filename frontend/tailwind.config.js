99bc41c491f72637d589939397e4318581d1fcf4
jvrtdev <joaovitorromanoteixeira@gmail.com>
2024-04-01 11:22:21 -0300

    :wrench: chore: config tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      '1-azul-metalico': '#1E3B75',
+      '2-cinza-prateado': '#CCCCCC',
+      '3-azul-claro': '#CEEDFF',
+      '4-dark' : '#333333',
+      '5-branco' : '#E7F2F4'
+    },  
+  },
+  plugins: [],
+}
