4a556fbe00e0a4fdf3c783a7d01ca9e1fa1dd29d
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-04-02 15:36:33 -0300

    :sparkles: feat: NavLink show the actual page

--- frontend/src/components/Header/Links.tsx
+++ frontend/src/components/Header/Links.tsx
@@ -1,21 +1,28 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { HeaderLink } from './@links';
 
 interface LinksProps {
   to: string;
-  name: string
+  name: string;
 }
 
 const Links: React.FC<LinksProps> = () => {
   return (
     <>
-    {HeaderLink.map((HeaderLink) => (
-      <Link 
-        key={HeaderLink.name} 
-        to={HeaderLink.to}   className="hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center hover:text-[#E7F2F4] text-[#CCCCCC] border-b border-transparent hover:border-[#CEEDFF] hover:border-b" >
-          {HeaderLink.name}
-        </Link>
-    ))}
+      {HeaderLink.map((headerLink) => (
+        <NavLink
+          key={headerLink.name}
+          to={headerLink.to}
+          style={({ isActive }) => ({
+            color: isActive ? 'green' : 'white',
+            borderBottom: isActive ? '2px solid green' : 'none', 
+          })}
+          className="hover:scale-105 transition-transform duration-300 ease-in-out transform origin-center hover:text-[#E7F2F4] text-[#CCCCCC] border-b border-transparent hover:border-[#CEEDFF] hover:border-b"
+        >
+          {headerLink.name}
+        </NavLink>
+      ))}
     </>
   );
 };
