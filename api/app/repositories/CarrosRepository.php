8fa041cccf14ce353fccc283920edba9d0fa54c1
jvrtdev <joaovitorromanoteixeira@gmail.com>
2024-04-02 15:42:18 -0300

    :sparkles: feat: add repository carro

---
+++ api/app/repositories/CarrosRepository.php
@@ -0,0 +1,28 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Database;
+use PDO;
+
+class CarrosRepository
+{   
+    protected $database;
+
+    public function __construct(Database $database)
+    {
+        $this->database = $database;
+    }
+
+    public function listarCarros(): array
+    {
+        $pdo = $this->database->getConnection();
+
+        $stmt = $pdo->query('SELECT * FROM carros');
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    //criar função com o nome listarCarrosPorId aqui
+}
