1b4bf17f0919dcc11c98811ecc430463c2db31bb
jvrtdev <joaovitorromanoteixeira@gmail.com>
2024-04-02 13:32:54 -0300

    :wrench: feat: add load do arquivo .env

--- api/public/index.php
+++ api/public/index.php
@@ -3,13 +3,16 @@ declare(strict_types=1);
 
 
 use App\Controllers\UserController;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 
 
+
+
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $app = AppFactory::create();
 
