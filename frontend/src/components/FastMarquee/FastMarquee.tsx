39fe72cc98af317d486f3621293f8011dace4f4e
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-04-02 21:32:27 -0300

    style: change speed in this fast marquee

--- frontend/src/components/FastMarquee/FastMarquee.tsx
+++ frontend/src/components/FastMarquee/FastMarquee.tsx
@@ -11,7 +11,7 @@ const FastMarquee: React.FC = () => {
       </div>
 
       <div className="w-full p-3">
-        <Marquee direction="right"  delay={1} speed={80}  gradientWidth={5} pauseOnClick >
+        <Marquee direction="right"  delay={1} speed={150}  gradientWidth={5} pauseOnClick >
           <div className="flex flex-col items-center">
             <img 
               src="/honda.png" 
